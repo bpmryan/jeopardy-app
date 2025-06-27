a69d5881b4425b462e1fcedc12750c09eea10571
bpmryandinh <bdinh@my.okcu.edu>
2025-06-26 20:46:02 -0500

    Added more code to submitButtonClick Supposed to open up the fiveCategory.fxml Doesn't work at the moment

--- jeopardy/src/main/java/com/example/jeopardyapp/QuestionApp.java
+++ jeopardy/src/main/java/com/example/jeopardyapp/QuestionApp.java
@@ -12,7 +12,7 @@ public class QuestionApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(QuestionApp.class.getResource("questionPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Question");
         stage.setScene(scene);
         stage.show();
     }
--- jeopardy/src/main/java/com/example/jeopardyapp/QuestionController.java
+++ jeopardy/src/main/java/com/example/jeopardyapp/QuestionController.java
@@ -2,9 +2,18 @@ package com.example.jeopardyapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class QuestionController {
+
+    private Stage stage;
+
     @FXML
     private Label welcomeText;
 
@@ -16,6 +25,13 @@ public class QuestionController {
     public void finalJeopardyButtonClick(ActionEvent actionEvent) {
     }
 
-    public void submitButtonClick(ActionEvent actionEvent) {
+    public void submitButtonClick(javafx.event.ActionEvent ActionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(QuestionApp.class.getResource("questionPage.fxml"));
+        stage = (Stage)((Node)ActionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Question Grid");
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
