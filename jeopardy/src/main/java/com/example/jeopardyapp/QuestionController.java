4bceb2483ed6d9e27ea214b4fe2a887404610760
bpmryandinh <bdinh@my.okcu.edu>
2025-08-17 21:44:12 -0500

    Messed around with the questionController.java and questionPage.fxml

--- jeopardy/src/main/java/com/example/jeopardyapp/QuestionController.java
+++ jeopardy/src/main/java/com/example/jeopardyapp/QuestionController.java
@@ -29,6 +29,8 @@ public class QuestionController {
 
     //opens the Question Board scene
     public void submitButtonClick(javafx.event.ActionEvent ActionEvent) throws IOException {
+        //create the question board grid through the questionPage
+
         FXMLLoader fxmlLoader = new FXMLLoader(QuestionApp.class.getResource("fiveCategories.fxml"));
         stage = (Stage)((Node)ActionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
